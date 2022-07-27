4ca46d7747a8636b90a416e200c2717482573f3f
wpark95 <park.wongyun@gmail.com>
2022-07-27 12:45:53 -0700

    Implemented parameterized tests w/ spec change.

--- src/main/java/com/tlglearning/fizzbuzz/model/Analysis.java
+++ src/main/java/com/tlglearning/fizzbuzz/model/Analysis.java
@@ -5,7 +5,20 @@ import java.util.Set;
 
 public class Analysis {
 
-  public Set<State> analyze(int value) {
+  private static final String ILLEGAL_VALUE_FORMAT = "Value was %,d; must be non-negative";
+
+  /**
+   *
+   * @param value
+   * @return
+   * @throws IllegalArgumentException if {@code value < 0}.
+   */
+
+  public Set<State> analyze(int value) throws IllegalArgumentException {
+
+    if (value < 0) {
+      throw new IllegalArgumentException(String.format(ILLEGAL_VALUE_FORMAT, value));
+    }
 
     Set<State> result = EnumSet.noneOf(State.class);
 
---
+++ src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
@@ -0,0 +1,57 @@
+package com.tlglearning.fizzbuzz.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.TexturePaint;
+import java.util.EnumSet;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class AnalysisTest {
+
+  static final Set<State> fizzExpected = EnumSet.of(State.FIZZ);
+  static final Set<State> fizzBuzzExpected = EnumSet.of(State.BUZZ, State.FIZZ);
+  static final Set<State> buzzExpected = EnumSet.of(State.BUZZ);
+  static final Set<State> neitherExpected = EnumSet.noneOf(State.class); // Empty set
+
+  final Analysis analysis = new Analysis();
+
+  @ParameterizedTest
+  @ValueSource(ints = {3, 21, 999_999_999})
+  void analyze_fizz(int value) {
+    assertEquals(fizzExpected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {0, 15, 999_999_990})
+  void analyze_fizzBuzz(int value) {
+    assertEquals(fizzBuzzExpected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {5, 85, 999_999_985})
+  void analyze_buzz(int value) {
+    assertEquals(buzzExpected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "neither.csv", numLinesToSkip = 1)
+  void analyze_neither(int value) {
+    assertEquals(neitherExpected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-1, -3, -5, -15})
+  void analyze_negative(int value) {
+    try {
+      analysis.analyze(value);
+      fail();
+    } catch (IllegalArgumentException e) {
+      // Do nothing; this is the expected behavior.
+    }
+  }
+
+}
