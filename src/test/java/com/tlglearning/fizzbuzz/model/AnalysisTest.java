48cc92349e8f03f80448b2908262737a39ae2272
wpark95 <park.wongyun@gmail.com>
2022-07-29 12:13:17 -0700

    TMFKA InvalidInvocation is now anonymous.

--- src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
+++ src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
@@ -45,21 +45,14 @@ class AnalysisTest {
   @ParameterizedTest
   @ValueSource(ints = {-1, -3, -5, -15})
   void analyze_negative(int value) {
-    assertThrows(IllegalArgumentException.class, new InvalidInvocation(value));
+    assertThrows(IllegalArgumentException.class, new Executable() {
+
+      @Override
+      public void execute() throws Throwable {
+        analysis.analyze(value);
+      }
+
+    });
   }
 
-  class InvalidInvocation implements Executable {
-
-    private final int value;
-
-    public InvalidInvocation(int value) {
-      this.value = value;
-    }
-
-    @Override
-    public void execute() throws Throwable {
-      analysis.analyze(value);
-    }
-
-  }
 }
